e2541935ad36e6eb7d938b92c6c628a22384fa8f
Juan Manuel Ojeda <jojeda@jumotech.com>
2022-03-11 20:19:46 +0100

    call_register is removed

--- call_register/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import call_register
-from . import ir_config_settings
-from . import res_users
-from . import call_dashboard
--- call_register/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import synchronization_calls_wzd
